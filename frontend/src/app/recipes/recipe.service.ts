30e1cc649b025d5ffa4baa7de876637bad0d3dce
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-11-30 15:53:32 +0100

    adding ingredients to recipes

--- frontend/src/app/recipes/recipe.service.ts
+++ frontend/src/app/recipes/recipe.service.ts
@@ -1,12 +1,32 @@
 import { Recipe } from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is just a test.', 'https://budapestflow.b-cdn.net/wp-content/uploads/2017/03/l%C3%A1ngos_street-kitchen.hu_.jpg'),
-    new Recipe('Another Test Recipe', 'This is just a test.', 'https://budapestflow.b-cdn.net/wp-content/uploads/2017/03/l%C3%A1ngos_street-kitchen.hu_.jpg')
+    new Recipe(
+      'Lángos',
+      'Super-tasty Hungarian dish.',
+      'https://budapestflow.b-cdn.net/wp-content/uploads/2017/03/l%C3%A1ngos_street-kitchen.hu_.jpg',
+      [
+        new Ingredient('Flour', 1),
+        new Ingredient('Oil', 1),
+        new Ingredient('Sour cream', 1),
+        new Ingredient('Cheese', 1),
+      ]),
+    new Recipe(
+      'Hamburger',
+      'This is huge hamburger.',
+      'https://blog.tefal.hu/webtemp/files/gallery/1/3/2690/1170_658_100__exact__large-actifry-genius-xl-2in1.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Onion', 1),
+        new Ingredient('Tomato', 1),
+        new Ingredient('Mustard', 1),
+        new Ingredient('Buns', 2),
+      ])
   ];
 
   getRecipes() {
