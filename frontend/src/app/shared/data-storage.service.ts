51137830558aee4114998bbc15fe1ce2134270de
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-12-20 17:36:50 +0100

    transforming response data, resolving data before loading

--- frontend/src/app/shared/data-storage.service.ts
+++ frontend/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs";
+import {map, tap} from "rxjs";
 
 @Injectable({providedIn: 'root'}) //instead of writing it in the app modules providers
 export class DataStorageService {
@@ -19,15 +19,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://recipe-app-2a54d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
       .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-        });
-      }))
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-      });
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
